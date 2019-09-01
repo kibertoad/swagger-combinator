9c7fdf3a5fb2ae1cf44d3eba3a4629bd58dd1326
Igor Savin <iselwin@gmail.com>
2019-09-01 23:58:11 +0200

    Add example script

---
+++ scripts/swaggerCombine.js
@@ -0,0 +1,11 @@
+const { OpenApi3SpecificationCombiner } = require('swagger-combinator')
+const combiner = new OpenApi3SpecificationCombiner()
+
+const TARGET_SPEC_FILE = '../serviceSpec.yaml'
+
+function combine() {
+  const mergedSpec = combiner.generateMergedSpec('../lib/**/api/**/*.yaml')
+  combiner.writeSpecAsYaml(mergedSpec, TARGET_SPEC_FILE)
+}
+
+combine()
